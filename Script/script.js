5a82a967b15efebe90ef2e69ecbcc6fdb9d8b1f1
hrantgartley <doyouevenjump@gmail.com>
2024-03-18 19:56:20 -0500

    fail path

--- Script/script.js
+++ Script/script.js
@@ -4,11 +4,11 @@ $(() => {
         type: "GET",
         dataType: "json",
         success: function (games) {
-            console.log("JSON Data:", games) // Check if JSON data is loaded correctly
+            console.log("JSON Data:", games); // Check if JSON data is loaded correctly
 
             // Iterate over the JSON array
             $.each(games, function (index, item) {
-                console.log("Item:", item) // Check each item in the iteration
+                console.log("Item:", item); // Check each item in the iteration
 
                 // Iterate over the array of objects within the item array
                 $.each(item, function (innerIndex, gameData) {
@@ -22,21 +22,24 @@ $(() => {
                           <p class="text-white">Created by: ${gameData.author}</p>
                           <br>
                       </div>
-                  </div>`
+                  </div>`;
 
-                    console.log("Generated HTML:", el) // Check generated HTML
+                    console.log("Generated HTML:", el); // Check generated HTML
 
                     // Append the HTML element to the container
-                    $("#firstCard").append(el)
+                    $("#firstCard").append(el);
 
                     // Append the author to the navigation dropdown
                     $(".nav-dropdown").append(
                         $(
                             `<li><a href="#!" class="dropdown-item">${gameData.author}</a></li>`,
                         ),
-                    )
-                })
-            })
+                    );
+                });
+            });
         },
-    })
-})
+        fail: () => {
+            console.log("An error occurred");
+        },
+    });
+});
